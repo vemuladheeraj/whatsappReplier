7abf79fdd78ccdf0ed86a892b7b301b1049e8631
Dheeraj Vemula <vemula.dheeraj@outlook.com>
2020-05-16 20:52:15 +0530

    Changed the logic of message trigger based on the time to Instant reply mechanism

--- messagesReplier.py
+++ messagesReplier.py
@@ -28,70 +28,61 @@ session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
 options = webdriver.ChromeOptions()
 options.add_argument(r"user-data-dir=C:\Users\vemul\PycharmProjects\whatsappReplier\Debug Files")
 # options.binary_location="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+# options.add_argument("--headless")
 
-driver=webdriver.Chrome(executable_path="D:\Selenium Learning\chromedriver_win32 (2)\chromedriver.exe", options=options)
+driver = webdriver.Chrome(executable_path="D:\Selenium Learning\chromedriver_win32 (2)\chromedriver.exe",
+                          options=options)
 driver.get("https://web.whatsapp.com/")
 driver.fullscreen_window()
+last_message_User = {}
+mycontacts = ['Sister']
+for name in mycontacts:
+    last_message_User[name] = ""
+wait = WebDriverWait(driver, 5000)
+wait.until(EC.element_to_be_clickable((By.ID, "side")))
+SystemLastDefaultMessage = "DefaultMessage"
 
-mycontacts={'Sister'}
-wait=WebDriverWait(driver,5000)
-wait.until(EC.element_to_be_clickable((By.ID,"side")))
-SystemLastDefaultMessage="DefaultMessage"
-while(True):
-     #unreadMessages=driver.find_elements_by_class_name("OUeyt")
-     #unreadFirstMessage=unreadMessages[0]
-     #if(unreadFirstMessage.text!=""):
-         #countOfUnreadMessage = int(unreadMessages[0].text) #if(countOfUnreadMessage>=0):
-     if(True):#will implement the validation logic later
-        for contact in mycontacts:
-            driver.find_element_by_xpath("//*[@id='side']/div[1]/div/label/div/div[2]").clear()
-            driver.find_element_by_xpath("//*[@id='side']/div[1]/div/label/div/div[2]").send_keys(contact)
-            time.sleep(1)
-            driver.find_element_by_xpath("//*[@id='side']/div[1]/div/label/div/div[2]").send_keys(Keys.ARROW_DOWN)
-            time.sleep(1)
-            allReceivedMessages = driver.find_elements_by_xpath("//*[contains(@class,'message-in focusable-list-item')]")
-            countOfOutMessages = len(allReceivedMessages)
-            countOfOutMessages=countOfOutMessages-1
-            lastMessageReceivedwithTime=allReceivedMessages[countOfOutMessages].text
-            lastMessageReceived=lastMessageReceivedwithTime.split("\n")[0]
-            lastMessageReceivedTime = lastMessageReceivedwithTime.split("\n")[1]
-            allSentMessages = driver.find_elements_by_xpath("//*[contains(@class,'message-out focusable-list-item')]")
-            countOfInMessages = len(allSentMessages)
-            lastMessageSentwithTime = allSentMessages[countOfInMessages - 1].text
-            lastMessageSent = lastMessageSentwithTime.split("\n")[0]
-            lastMessageSentTime = lastMessageSentwithTime.split("\n")[1]
-            currentDT = datetime.datetime.now()
-            if(lastMessageReceived!=SystemLastDefaultMessage):
-                SystemLastDefaultMessage=lastMessageReceived
-                if (lastMessageReceivedTime.split(" ")[1] == lastMessageSentTime.split(" ")[1]):
-                    if (lastMessageReceivedTime.split(" ")[0] >= lastMessageSentTime.split(" ")[0]):
-                        # ned to send this lastMessageReceived
-                        text_to_be_analyzed = lastMessageReceived
-                        text_input = dialogflow.types.TextInput(text=text_to_be_analyzed,
-                                                                language_code=DIALOGFLOW_LANGUAGE_CODE)
-                        query_input = dialogflow.types.QueryInput(text=text_input)
-                        try:
-                            response = session_client.detect_intent(session=session, query_input=query_input)
-                        except InvalidArgument:
-                            raise
-                        responseForMessage = response.query_result.fulfillment_text
-                        driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[2]/div/div[2]").send_keys(
-                            responseForMessage)
-                        driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[2]/div/div[2]").send_keys(
-                            Keys.ENTER)
-                    # elif(lastMessageReceivedTime.split(" ")[1] =="PM" & lastMessageSentTime.split(" ")[1]=="AM"):  #PM =6.45 pm , am = 9.50 am
-                else:
-                    driver.find_element_by_xpath(
-                        "//*[contains(@class,'message-out focusable-list-item')][last()]/div/div/div/div[2]/div/div/span").click()
-                    time.sleep(2)
-                    driver.find_element_by_xpath("//*[@id='app']/div/span[4]/div/ul/li[1]/div").click()
-                    time.sleep(2)
-                    deliveredDate_Self = driver.find_element_by_xpath(
-                        "//*[@id='app']/div/div/div[2]/div[3]/span/div/span/div/div[2]/div/div/div[2]/div/div[2]/div").Text
 
-                    current_median = currentDT.strftime("%p")  # pm
-                    current_date = currentDT.strftime("%I:%M:%S %p")  # 04:13:59 PM
-                    if (current_median == "PM"):
-                        if (currentDT.strftime("%I%M") < lastMessageReceivedTime.split(" ")[1]):
-                            I_Need_Give_Reply = "One method"
-                            # I need to give reply currentDT.strftime("%I:%M:%S %p"))
+def addSecs(tm, secs):
+    fulldate = datetime.datetime(100, 1, 1, tm.hour, tm.minute, tm.second)
+    fulldate = fulldate + datetime.timedelta(seconds=secs)
+    return fulldate.time()
+
+
+for contact in mycontacts:
+    driver.find_element_by_xpath("//*[@id='side']/div[1]/div/label/div/div[2]").clear()
+    driver.find_element_by_xpath("//*[@id='side']/div[1]/div/label/div/div[2]").send_keys(contact)
+    time.sleep(1)
+    driver.find_element_by_xpath("//*[@id='side']/div[1]/div/label/div/div[2]").send_keys(Keys.ARROW_DOWN)
+    time.sleep(1)
+    lastMessageReceivedwithTime = driver.find_element_by_xpath(
+        "//*[contains(@class,'message-in focusable-list-item')][last()]").text
+    Temp_lastMessageReceived = lastMessageReceivedwithTime.split("\n")[0]
+    last_message_User[contact] = Temp_lastMessageReceived
+    before_loop_date_time = datetime.datetime.now().time()
+    loop_break_time = addSecs(before_loop_date_time, 60)
+    while True:
+        current_last_message_received = \
+            driver.find_element_by_xpath("//*[contains(@class,'message-in focusable-list-item')][last()]").text.split(
+                "\n")[
+                0]
+        if current_last_message_received != last_message_User.get(contact):
+            text_to_be_analyzed = current_last_message_received
+            last_message_User[contact] = current_last_message_received
+            text_input = dialogflow.types.TextInput(text=text_to_be_analyzed,
+                                                    language_code=DIALOGFLOW_LANGUAGE_CODE)
+            query_input = dialogflow.types.QueryInput(text=text_input)
+            try:
+                response = session_client.detect_intent(session=session, query_input=query_input)
+            except InvalidArgument:
+                raise
+            responseForMessage = response.query_result.fulfillment_text
+            driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[2]/div/div[2]").send_keys(
+                responseForMessage)
+            driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[2]/div/div[2]").send_keys(
+                Keys.ENTER)
+            before_loop_date_time = datetime.datetime.now().time()
+            loop_break_time = addSecs(before_loop_date_time, 40)
+        elif datetime.datetime.now().time() > loop_break_time:
+            break
+driver.close()
